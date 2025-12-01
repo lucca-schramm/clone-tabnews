a945fe64124e874d04408118f5b920a0db5add59
Lucca Schramm <112810568+lucca-schramm@users.noreply.github.com>
2025-12-01 12:13:05 +0000

    first lint

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação do htts://www.tabnews.com.br para o https://curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi paixão! Te amo muito!</h1>
+  return <h1>Oi paixão! Te amo muito!</h1>;
 }
 
 export default Home;
